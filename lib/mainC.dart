130097829711d34b748b9f004c7450885bfd018b
Camila Pinheiro <102990733+mcsp3@users.noreply.github.com>
2023-08-01 11:02:49 -0300

    mainC.dart

---
+++ lib/mainC.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:aula_923/Refeicoes.dart';
+
+void main() {
+
+  runApp(
+    MaterialApp(
+
+      home: Refeicoes(),
+      debugShowCheckedModeBanner: false ,
+
+    ),
+  );
+}
