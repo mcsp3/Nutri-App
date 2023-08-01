c39e89438bdd1cc3f3e0afe257277dc5c1a85430
Camila Pinheiro <102990733+mcsp3@users.noreply.github.com>
2023-08-01 10:53:59 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:aula_913/pages/tela_principal.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: TelaPrincipal()
+    ),
+  );
+}
